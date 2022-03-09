49c90ada910101235007accba64e1050362de0ac
cankirism <cankiri.bilsis@saglik.gov.tr>
2022-03-09 13:24:59 +0300

    Add project files.

---
+++ CICDSample/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using CICDSample.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CICDSample.Controllers
+{
+    [Route("api/v1/Product/")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            var products = new List<Product>() {
+
+                new Product(){Id=1,Name="TV",Piece=20},
+                new Product(){ Id=2,Name="PC",Piece=10}
+
+
+            };
+            return products;
+
+        }
+    }
+
+
+}
+
+
